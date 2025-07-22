e12f9e141f4cc5a7ba5e68dd15dc47e005fe4bc1
Arif Tunçer <arif.61.tuncer@gmail.com>
2025-07-23 02:51:51 +0300

    feat(ui) main acitivity and its navigation fragments added

--- app/src/main/java/com/vedatturkkal/stajokulu2025yoklama/ui/main/MainActivity.kt
+++ app/src/main/java/com/vedatturkkal/stajokulu2025yoklama/ui/main/MainActivity.kt
@@ -1,22 +1,59 @@
 package com.vedatturkkal.stajokulu2025yoklama.ui.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.vedatturkkal.stajokulu2025yoklama.Home
 import com.vedatturkkal.stajokulu2025yoklama.R
+import com.vedatturkkal.stajokulu2025yoklama.databinding.ActivityMainBinding
 
 //navigasyonun ve ana view modelin aktivitesi.
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view : View = binding.root
+        setContentView(binding.root)
+        setSupportActionBar(binding.materialToolbar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        replaceFragment(HomeFragment())
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.action_home -> replaceFragment(HomeFragment())
+                R.id.action_takeAttendance -> replaceFragment(AttendanceFragment())
+                R.id.action_attendanceList -> replaceFragment(AttendanceFragment())
+                R.id.action_settings -> replaceFragment(SettingsFragment())
+                else -> true
+            }
+            true
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.mainFrameLayout,fragment)
+        fragmentTransaction.commit()
     }
 }
+
